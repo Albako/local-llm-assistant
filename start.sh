d1c97376f6885d25c82a5b39d54ca9bee6f7131d
Tmseh <t.sawicki00@onet.pl>
2025-06-13 22:38:04 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
-npm install 
-npm run dev 
+#!/usr/bin/env bash
+# start.sh – uruchamia lokalny serwer FastAPI
+
+set -e
+# Źródło zmiennych środowiskowych (jeśli jest plik .env)
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+uvicorn app.main:app --reload --host $API_HOST --port $API_PORT
