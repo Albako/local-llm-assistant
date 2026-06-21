f8074befb5a96b0a9b54a7ffca1b4aa78a62b804
Konrad Lesny <konradl.kontakt@pm.me>
2026-06-21 15:50:56 +0200

    Add SvelteKit PWA plugin and clean up config
    Removed commented-out server configuration and added SvelteKit PWA plugin with options.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-
-// /** @type {import('vite').Plugin} */
-// const viteServerConfig = {
-// 	name: 'log-request-middleware',
-// 	configureServer(server) {
-// 		server.middlewares.use((req, res, next) => {
-// 			res.setHeader('Access-Control-Allow-Origin', '*');
-// 			res.setHeader('Access-Control-Allow-Methods', 'GET');
-// 			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-// 			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-// 			next();
-// 		});
-// 	}
-// };
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
 	plugins: [
@@ -28,7 +14,18 @@ export default defineConfig({
 					dest: 'wasm'
 				}
 			]
-		})
+		}),
+		SvelteKitPWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
+            manifest: false,
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,json,woff,woff2,ttf}'],
+                navigateFallbackDenylist: [/^\/api/]
+            }
+        })
 	],
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
