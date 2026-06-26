21c8a0db461fc7aef326e4574181fb5ddc9d3db6
Konrad Lesny <konradl.kontakt@pm.me>
2026-06-26 21:22:42 +0200

    Update server URL placeholder in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,7 @@ const config: CapacitorConfig = {
   	webDir: 'build',
   	server: {
     	androidScheme: 'https',
-    	url: 'http://TWÓJ_ADRES_IP:3000',
+    	url: 'http://<IP address of the server with backend>:3000',
     	cleartext: true
   	},
 	plugins: {
